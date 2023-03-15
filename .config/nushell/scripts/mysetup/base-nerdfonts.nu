e533b39423493e84c4c2e866baf49578721d9c96
Benoit Benedetti <benoit.benedetti@gmail.com>
2023-03-15 15:02:40 +0100

    Switch to FiraCode font

--- .config/nushell/scripts/mysetup/base-nerdfonts.nu
+++ .config/nushell/scripts/mysetup/base-nerdfonts.nu
@@ -1,11 +1,11 @@
 # Install Nerd Fonts
 def "mysetup base nerdfonts" [] { 
 
-    let font = 'JetBrainsMono'
+    let font = 'FiraCode'
     echo $"Installing Nerd Fonts ($font)"
     mkdir $"/tmp/nerd-fonts/patched-fonts/($font)"
-    github latestdownloads ryanoasis/nerd-fonts | where $it =~ $font | to text | curl -sSL $in -o $"/tmp/($font).zip"
-    unzip -x  /tmp/JetBrainsMono.zip -d $"/tmp/nerd-fonts/patched-fonts/($font)/"
+    github latestdownloads ryanoasis/nerd-fonts | where $it =~ $font | curl -sSL $in.0 -o /tmp/font.zip
+    unzip -x $"/tmp/font.zip" -d $"/tmp/nerd-fonts/patched-fonts/($font)/"
     cd /tmp/nerd-fonts
     wget -q https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/install.sh
     bash install.sh $font
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,23 +3,19 @@ local wezterm = require("wezterm")
 return {
 	enable_tab_bar = true,
 	hide_tab_bar_if_only_one_tab = true,
-
-	font = wezterm.font("JetBrainsMono Nerd Font Mono"),
-
+	font = wezterm.font("FiraCode Nerd Font"),
 	color_scheme = "nord",
-
 	inactive_pane_hsb = {
 		saturation = 0.9,
 		brightness = 0.7,
 	},
-
 	disable_default_key_bindings = true,
 	-- timeout_milliseconds defaults to 1000 and can be omitted
 	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
 	keys = {
 		-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
 		{ key = "a", mods = "LEADER|CTRL", action = wezterm.action({ SendString = "\x01" }) },
-		{ key = "t", mods = "LEADER", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
+		{ key = "t", mods = "LEADER",      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
 		{
 			key = "o",
 			mods = "LEADER",
@@ -30,22 +26,22 @@ return {
 			mods = "LEADER",
 			action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }),
 		},
-		{ key = "h", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-		{ key = "l", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-		{ key = "j", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-		{ key = "k", mods = "LEADER", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-		{ key = "f", mods = "LEADER", action = wezterm.action({ Search = { CaseSensitiveString = "" } }) },
-		{ key = "-", mods = "LEADER", action = "DecreaseFontSize" },
-		{ key = "_", mods = "LEADER", action = "IncreaseFontSize" },
-		{ key = "=", mods = "LEADER", action = "ResetFontSize" },
-		{ key = "w", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-		{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
-		{ key = "PageUp", mods = "SHIFT", action = wezterm.action({ ScrollByPage = -1 }) },
-		{ key = "PageDown", mods = "SHIFT", action = wezterm.action({ ScrollByPage = 1 }) },
-		{ key = "Tab", mods = "CTRL", action = wezterm.action({ ActivateTabRelative = 1 }) },
-		{ key = "Tab", mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+		{ key = "h",        mods = "LEADER",     action = wezterm.action({ ActivatePaneDirection = "Left" }) },
+		{ key = "l",        mods = "LEADER",     action = wezterm.action({ ActivatePaneDirection = "Right" }) },
+		{ key = "j",        mods = "LEADER",     action = wezterm.action({ ActivatePaneDirection = "Down" }) },
+		{ key = "k",        mods = "LEADER",     action = wezterm.action({ ActivatePaneDirection = "Up" }) },
+		{ key = "f",        mods = "LEADER",     action = wezterm.action({ Search = { CaseSensitiveString = "" } }) },
+		{ key = "-",        mods = "LEADER",     action = "DecreaseFontSize" },
+		{ key = "_",        mods = "LEADER",     action = "IncreaseFontSize" },
+		{ key = "=",        mods = "LEADER",     action = "ResetFontSize" },
+		{ key = "w",        mods = "LEADER",     action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+		{ key = "z",        mods = "LEADER",     action = "TogglePaneZoomState" },
+		{ key = "PageUp",   mods = "SHIFT",      action = wezterm.action({ ScrollByPage = -1 }) },
+		{ key = "PageDown", mods = "SHIFT",      action = wezterm.action({ ScrollByPage = 1 }) },
+		{ key = "Tab",      mods = "CTRL",       action = wezterm.action({ ActivateTabRelative = 1 }) },
+		{ key = "Tab",      mods = "CTRL|SHIFT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+		{ key = "c",        mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+		{ key = "v",        mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
 		{
 			key = "LeftArrow",
 			mods = "CTRL|SHIFT",
